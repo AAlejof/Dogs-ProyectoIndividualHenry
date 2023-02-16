a7187d3a0ea63f0521d714be745b6ad18a049d13
AAlejof <scaramouch11@gmail.com>
2023-02-15 22:57:26 -0300

    models and DB ok

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -3,10 +3,34 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
+  sequelize.define('Dog', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }, 
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }, 
+    lifeSpan: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    timestamps: false,
   });
 };
