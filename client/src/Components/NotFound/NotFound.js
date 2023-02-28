76c382856390118053661727e20232d842efca68
AAlejof <scaramouch11@gmail.com>
2023-02-27 22:36:49 -0300

    change divs order - fix img import

--- client/src/Components/NotFound/NotFound.js
+++ client/src/Components/NotFound/NotFound.js
@@ -1,14 +1,14 @@
 import style from './NotFound.module.css';
-import image from '../Img/notfound.png'
+import image from '../../assets/Img/notfound.png'
 
 const NotFound = () =>{
     return(
         <div className={style.NotFoundContainer}>
-            <div className={style.NotFoundDescription}>
-                <h2>Not found! To tired to work...</h2>
-            </div>
             <div>
                 <img src={image} alt='Golden retriever'/>
+            </div>
+            <div className={style.NotFoundDescription}>
+                <h2>Not found! To tired to work...</h2>
             </div>
         </div>
     )
