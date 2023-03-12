b292c83ef0280220bbb5d6deb8c231e37bb69653
AAlejof <scaramouch11@gmail.com>
2023-03-11 22:27:16 -0300

    import action types

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,14 +1,15 @@
+import  { GET_DOGS,
+    GET_DOGS_BY_NAME,
+    GET_DOG_DETAIL,
+    GET_TEMPERAMENTS,
+    CREATE_DOG,
+    FILTER_BY_TEMP,
+    FILTER_BY_STORED,
+    SORT_BY_WEIGHT,
+    SORT_BY_NAME,
+    CLEAR_DOG_DETAIL 
+    } from "./action.types";
 import axios from 'axios';
-export const GET_DOGS = 'GET_DOGS';
-export const GET_DOGS_BY_NAME = 'GET_DOGS_BY_NAME';
-export const GET_DOG_DETAIL = 'GET_DOG_DETAIL';
-export const GET_TEMPERAMENTS = 'GET_TEMPERAMENTS';
-export const CREATE_DOG = 'CREATE_DOG';
-export const FILTER_BY_TEMP = 'FILTER_BY_TEMP';
-export const FILTER_BY_STORED = 'FILTER_BY_STORED';
-export const SORT_BY_WEIGHT = 'SORT_BY_WEIGHT';
-export const SORT_BY_NAME = 'SORT_BY_NAME';
-export const CLEAR_DOG_DETAIL = 'CLEAR_DOG_DETAIL';
 
 export function getDogs(name){
     return async function(dispatch){
