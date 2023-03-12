c04e40b004593c70422ef9c3236def3a8abf6412
AAlejof <scaramouch11@gmail.com>
2023-03-11 22:27:00 -0300

    modularize action types

---
+++ client/src/redux/action.types.js
@@ -0,0 +1,11 @@
+export const GET_DOGS = 'GET_DOGS';
+export const GET_DOGS_BY_NAME = 'GET_DOGS_BY_NAME';
+export const GET_DOG_DETAIL = 'GET_DOG_DETAIL';
+export const GET_TEMPERAMENTS = 'GET_TEMPERAMENTS';
+export const CREATE_DOG = 'CREATE_DOG';
+export const FILTER_BY_TEMP = 'FILTER_BY_TEMP';
+export const FILTER_BY_STORED = 'FILTER_BY_STORED';
+export const SORT_BY_WEIGHT = 'SORT_BY_WEIGHT';
+export const SORT_BY_NAME = 'SORT_BY_NAME';
+export const CLEAR_DOG_DETAIL = 'CLEAR_DOG_DETAIL';
+
